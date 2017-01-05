905286dc6e752e48d9df47be61a53a65146bd75a
Pooja <pthakare33@gmail.com>
2017-01-04 18:35:55 -0800

    es6

--- es6server.js
+++ es6server.js
@@ -1,11 +1,12 @@
-var express = require('express'),
-    http = require('http'),
-    path = require('path'),
-    bodyParser = require('body-parser');
-cors = require('cors');
-var app = express();
-var mysql = require('mysql');
-var md5 = require('md5');
+"use strict";
+import express      from 'express';
+import path         from 'path';
+import http         from 'http';
+import bodyParser   from 'body-parser';
+import cors         from 'cors';
+let app = express();
+import mysql         from 'mysql';
+import md5         from 'md5';
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -21,7 +22,6 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing       application/x-www-form-urlencoded
 
 
-
 app.post('/view1',  (req, res) => {
     var name = req.body.name;
     var email = req.body.email;
@@ -32,6 +32,8 @@ app.post('/view1',  (req, res) => {
     res.end();
 });
 
+
+
 function insertData(body) {
     console.log(body);
     connection.query('INSERT INTO people (name) VALUES (?)', [body.name], function (err, result) {
@@ -40,6 +42,9 @@ function insertData(body) {
     });
 }
 
-http.createServer(app).listen(app.get('port'), function () {
+http.createServer(app).listen(app.get('port'),  ()=> {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+module.exports = app;
